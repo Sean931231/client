e6d76f7ff39baf89629be8fc59f2afdaae762743
Sean Wong <seanwonghh93@gmail.com>
2021-12-16 11:39:33 +0800

    Update
    - Update part 1 section of the course

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <HeroComponents />
+  </div>
+</template>
+
+<script>
+import HeroComponents from '~/components/HeroComponents.vue'
+
+export default {
+  name: 'IndexPage',
+  components: { HeroComponents }
+}
+</script>
