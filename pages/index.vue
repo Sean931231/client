659ae12c74ae39b3e63f29a6babe219fbcdb25d9
Sean Wong <seanwonghh93@gmail.com>
2021-12-20 10:46:47 +0800

    update
    - add large card display with contents in looping

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="container">
     <HeroComponents />
+    <div style="height: 150px"></div>
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
   </div>
 </template>
 
 <script>
 import HeroComponents from '~/components/HeroComponents.vue'
+import LargeCardDisplay from '~/components/LargeCardDisplay.vue'
+import { largeCardSections } from '@/assets/data.js'
 
 export default {
   name: 'IndexPage',
-  components: { HeroComponents }
+  components: { HeroComponents, LargeCardDisplay },
+  data () {
+    return {
+      largeCardInfo: largeCardSections
+    }
+  }
 }
 </script>
